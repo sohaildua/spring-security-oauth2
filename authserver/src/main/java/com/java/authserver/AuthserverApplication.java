a570213cf32529ae7c243eff74d7a2e734670c5b
dua <sohail.dua@webfleet.com>
2023-12-11 03:52:30 +0100

    authserver

---
+++ authserver/src/main/java/com/java/authserver/AuthserverApplication.java
@@ -0,0 +1,14 @@
+package com.java.authserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthserverApplication {
+
+	public static void main(final String[] args) {
+		SpringApplication.run(AuthserverApplication.class, args);
+	}
+
+}
+ 
